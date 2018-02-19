e29b9c37c5a264b927af5243c912c28946e39039
shreya aglave <36595287+shreyaaglave@users.noreply.github.com>
2018-02-19 21:56:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/', function (req, res){
 });
 
 app.get('/:articleName',function(req,res){
-    var articleName=req.params;
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName])); 
 });
 
