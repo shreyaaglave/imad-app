575a7fed34f445703b04a3f42e377806e512e3e9
shreya aglave <36595287+shreyaaglave@users.noreply.github.com>
2018-02-20 21:45:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'NEW VALUE';
 
 var img=document.getElementById('madi'); 
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
 img.onclick=function(){
+    var interval=setInterval(moveRight,100);
   img.style.marginLeft='100px';  
 };
