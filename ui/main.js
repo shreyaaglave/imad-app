3214a28634307a8d5c2d23dbc0ebbc8447fd35b0
shreya aglave <36595287+shreyaaglave@users.noreply.github.com>
2018-02-20 21:30:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'NEW VALUE';
+
+var img=document.getElementById('img');
+img.onclick=function(){
+  img.style.marginLeft='100px';  
+};
